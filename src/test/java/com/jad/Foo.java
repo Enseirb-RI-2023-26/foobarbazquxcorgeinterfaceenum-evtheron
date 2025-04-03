50cd5af0c8b4dcba751248e1781a7ff29e33d2ef
Evan THERON <evan.theron@free.fr>
2025-04-03 16:32:19 +0200

    first try

---
+++ src/test/java/com/jad/Foo.java
@@ -0,0 +1,45 @@
+package com.jad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Foo implements IFoo {
+    private IBaz baz;
+    private List<IBar> bars;
+    private IQux qux;
+    private ICorge corge;
+
+    public Foo(IBaz baz) {
+        this.baz = baz;
+        this.bars = new ArrayList<>();
+        this.qux = new Qux();
+    }
+
+    public void addBar(IBar bar) {
+        this.bars.add(bar);
+    }
+
+    public IBaz getBaz() {
+        return this.baz;
+    }
+
+    public List<IBar> getBars() {
+        return this.bars;
+    }
+
+    public IQux getQux() {
+        return this.qux;
+    }
+
+    @Override
+    public ICorge getCorge() {
+        return this.corge;
+    }
+
+    @Override
+    public void setCorge(ICorge corge) {
+        if (this.corge != null) {this.corge.setFoo(null);}
+        this.corge = corge;
+    }
+
+}
---
+++ src/test/java/com/jad/IFoo.java
@@ -0,0 +1,6 @@
+package com.jad;
+
+public interface IFoo {
+    public ICorge getCorge();
+    public void setCorge(ICorge corge);
+}
